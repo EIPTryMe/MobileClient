7cb1506f6e2907bb22e735eab920b12d5f7d78c5
supermario35 <mario.polart@hotmail.fr>
2020-12-08 19:51:51 +0100

    Fix

--- try_me/lib/views/ShoppingCardView.dart
+++ try_me/lib/views/ShoppingCardView.dart
@@ -57,287 +57,152 @@ class _ShoppingCardViewState extends State<ShoppingCardView> {
 
     return Padding(
       padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-      child: Stack(children: [
-        Container(
-          height: 125.0,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                width: 105,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(Styles.cardRadius),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 10.0, top: 20.0, bottom: 20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        cart.product.name,
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Styles.colors.text,
-                          fontWeight: FontWeight.w400,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        "$price€ / mois",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Styles.colors.text,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      Text(
-                        "$duration mois",
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Styles.colors.text,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
+      child: Stack(
+        children: [
+          Container(
+            height: 125.0,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  width: 105,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(Styles.cardRadius),
                   ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    height: 17,
-                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(
-                      color: Styles.colors.main,
-                      borderRadius: BorderRadius.all(Radius.circular(32.5)),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10.0, top: 20.0, bottom: 20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          cart.product.name,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Styles.colors.text,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          "$price€ / mois",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Styles.colors.text,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          "$duration mois",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Styles.colors.text,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
                     ),
-                    child: Center(
-                      child: Text(
-                        cart.quantity.toString(),
-                        style: TextStyle(
-                          color: Styles.colors.text,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w900,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 17,
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        color: Styles.colors.main,
+                        borderRadius: BorderRadius.all(Radius.circular(32.5)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          cart.quantity.toString(),
+                          style: TextStyle(
+                            color: Styles.colors.text,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w900,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              /*
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                /*
                               (if card.quantity > 1)
                               * */
-                              cart.quantity--;
-                              _total = NumberFormatTool.formatPrice(
-                                  shoppingCard.total);
-                            });
-                          },
-                          icon: Icon(
-                            Icons.remove,
-                            color: Styles.colors.text,
+                                cart.quantity--;
+                                _total = NumberFormatTool.formatPrice(
+                                    shoppingCard.total);
+                              });
+                            },
+                            icon: Icon(
+                              Icons.remove,
+                              color: Styles.colors.text,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        child: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              /*
+                        Container(
+                          child: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                /*
                               if (card.quantity < cart.product.stock)
                                */
-                              cart.quantity++;
-                              _total = NumberFormatTool.formatPrice(
-                                  shoppingCard.total);
-                            });
-                          },
-                          icon: Icon(
-                            Icons.add,
-                            color: Styles.colors.text,
+                                cart.quantity++;
+                                _total = NumberFormatTool.formatPrice(
+                                    shoppingCard.total);
+                              });
+                            },
+                            icon: Icon(
+                              Icons.add,
+                              color: Styles.colors.text,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete_forever),
-                    color: Styles.colors.unSelected,
-                    onPressed: () {
-                      setState(() => _loading = true);
-                      Request.deleteShoppingCard(cart.id).then((hasException) {
-                        getData();
-                      });
-                    },
-                  ),
-                ],
-              ),
-              Positioned.fill(
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 100.0),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(Styles.cardRadius),
-                      onTap: () => Navigator.pushNamed(
-                          context, 'product/${cart.product.id}'),
+                      ],
                     ),
-                  ),
+                    IconButton(
+                      icon: Icon(Icons.delete_forever),
+                      color: Styles.colors.unSelected,
+                      onPressed: () {
+                        setState(() => _loading = true);
+                        Request.deleteShoppingCard(cart.id)
+                            .then((hasException) {
+                          getData();
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned.fill(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 90.0),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(Styles.cardRadius),
+                  onTap: () => Navigator.pushNamed(
+                      context, 'product/${cart.product.id}'),
                 ),
               ),
-            ],
+            ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
-
-//    return Padding(
-//      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-//      child: Stack(
-//        children: [
-//          Container(
-//            height: 125.0,
-//            child: Row(
-//              crossAxisAlignment: CrossAxisAlignment.stretch,
-//              children: [
-//                Container(
-//                    width: 105,
-//                    decoration: BoxDecoration(
-//                      color: Colors.white,
-//                      borderRadius: BorderRadius.circular(Styles.cardRadius),
-//                    ),
-//                    child: cart.product.pictures.isEmpty
-//                        ? null
-//                        : ClipRRect(
-//                        borderRadius:
-//                        BorderRadius.circular(Styles.cardRadius),
-//                        child: Image.network(cart.product.pictures[0]))),
-////                Expanded(
-////                    child: Padding(
-////                      padding: const EdgeInsets.only(
-////                          left: 10.0, top: 20.0, bottom: 20.0),
-////                      child: Column(
-////                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-////                        crossAxisAlignment: CrossAxisAlignment.stretch,
-////                        children: [
-////                          Text(
-////                            cart.product.name,
-////                            style: TextStyle(
-////                              fontSize: 18.0,
-////                              color: Styles.colors.text,
-////                              fontWeight: FontWeight.w400,
-////                            ),
-////                            overflow: TextOverflow.ellipsis,
-////                          ),
-////                          Text(
-////                            "$price€ / mois",
-////                            style: TextStyle(
-////                              fontSize: 18.0,
-////                              color: Styles.colors.text,
-////                              fontWeight: FontWeight.w700,
-////                            ),
-////                          ),
-////                        ],
-////                      ),
-////                    )),
-////                Container(
-////                  width: 50,
-////                  child: Column(
-////                    mainAxisAlignment: MainAxisAlignment.end,
-////                    crossAxisAlignment: CrossAxisAlignment.stretch,
-////                    children: [
-////                      Expanded(
-////                          child: Row(
-////                            children: [
-////                              Container(
-////                                child: IconButton(
-////                                  onPressed: () {
-////                                    setState(() {
-////                                      cart.quantity--;
-////                                    });
-////                                  },
-////                                  icon: Icon(Icons.remove, color: Styles.colors.text,),
-////                                ),
-////                              ),
-////                              Container(
-////                                height: 17,
-////                                padding: const EdgeInsets.symmetric(
-////                                    horizontal: 5.0),
-////                                decoration: BoxDecoration(
-////                                  color: Styles.colors.main,
-////                                  borderRadius: BorderRadius.all(
-////                                      Radius.circular(32.5)),
-////                                ),
-////                                child: Center(
-////                                  child: Text(
-////                                    cart.quantity.toString(),
-////                                    style: TextStyle(
-////                                      color: Styles.colors.text,
-////                                      fontSize: 12,
-////                                      fontWeight: FontWeight.w900,
-////                                    ),
-////                                  ),
-////                                ),
-////                              ),
-////                              Container(
-////                                child: IconButton(
-////                                  onPressed: () {
-////                                    setState(() {
-////                                      cart.quantity++;
-////                                    });
-////                                  },
-////                                  icon: Icon(Icons.remove, color: Styles.colors.text,),
-////                                ),
-////                              ),
-////                            ],
-////                          ),
-////                      ),
-////                      Expanded(
-////                        child: IconButton(
-////                          icon: Icon(Icons.delete_forever),
-////                          color: Styles.colors.unSelected,
-////                          onPressed: () {
-////                            setState(() => _loading = true);
-////                            Request.deleteShoppingCard(cart.id)
-////                                .then((hasException) {
-////                              getData();
-////                            });
-////                          },
-////                        ),
-////                      ),
-//                    ],
-//                  ),
-//                ),
-//              ],
-//          //  ),
-//          ),
-////          Positioned.fill(
-////            child: Padding(
-////              padding: const EdgeInsets.only(right: 40.0),
-////              child: Material(
-////                color: Colors.transparent,
-////                child: InkWell(
-////                  borderRadius: BorderRadius.circular(Styles.cardRadius),
-////                  onTap: () =>
-////                      Navigator.pushNamed(
-////                          context, 'product/${cart.product.id}'),
-////                ),
-////              ),
-////            ),
-////          ),
-////        ],
-////      ),
-//    );
   }
 
   @override
