e4f96dfc2b72a5ac8c52bc445ec1b05ce1e20390
Auguste Larrive <augustelarrivejahan@gmail.com>
2020-12-08 19:38:53 +0100

    sho^pping card view nearly done ink well fail

--- try_me/lib/views/ShoppingCardView.dart
+++ try_me/lib/views/ShoppingCardView.dart
@@ -44,7 +44,7 @@ class _ShoppingCardViewState extends State<ShoppingCardView> {
           borderRadius: BorderRadius.circular(12.0),
         ),
         child: Text(
-          "Payer ($_total€)",
+          "Payer ($_total€ / mois)",
           style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w700),
         ),
       ),
@@ -53,30 +53,25 @@ class _ShoppingCardViewState extends State<ShoppingCardView> {
 
   Widget _shoppingCardCart(Cart cart) {
     String price = NumberFormatTool.formatPrice(cart.product.pricePerMonth);
+    String duration = cart.duration.toString();
 
     return Padding(
       padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-      child: Stack(
-        children: [
-          Container(
-            height: 125.0,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                    width: 105,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(Styles.cardRadius),
-                    ),
-                    child: cart.product.pictures.isEmpty
-                        ? null
-                        : ClipRRect(
-                            borderRadius:
-                                BorderRadius.circular(Styles.cardRadius),
-                            child: Image.network(cart.product.pictures[0]))),
-                Expanded(
-                    child: Padding(
+      child: Stack(children: [
+        Container(
+          height: 125.0,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 105,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Styles.cardRadius),
+                ),
+              ),
+              Expanded(
+                child: Padding(
                   padding: const EdgeInsets.only(
                       left: 10.0, top: 20.0, bottom: 20.0),
                   child: Column(
@@ -100,48 +95,249 @@ class _ShoppingCardViewState extends State<ShoppingCardView> {
                           fontWeight: FontWeight.w700,
                         ),
                       ),
-                    ],
-                  ),
-                )),
-                Container(
-                  width: 50,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.delete_forever),
-                        color: Styles.colors.unSelected,
-                        onPressed: () {
-                          setState(() => _loading = true);
-                          Request.deleteShoppingCard(cart.id)
-                              .then((hasException) {
-                            getData();
-                          });
-                        },
+                      Text(
+                        "$duration mois",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Styles.colors.text,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ],
-            ),
-          ),
-          Positioned.fill(
-            child: Padding(
-              padding: const EdgeInsets.only(right: 40.0),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(Styles.cardRadius),
-                  onTap: () => Navigator.pushNamed(
-                      context, 'product/${cart.product.id}'),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 17,
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(
+                      color: Styles.colors.main,
+                      borderRadius: BorderRadius.all(Radius.circular(32.5)),
+                    ),
+                    child: Center(
+                      child: Text(
+                        cart.quantity.toString(),
+                        style: TextStyle(
+                          color: Styles.colors.text,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              /*
+                              (if card.quantity > 1)
+                              * */
+                              cart.quantity--;
+                              _total = NumberFormatTool.formatPrice(
+                                  shoppingCard.total);
+                            });
+                          },
+                          icon: Icon(
+                            Icons.remove,
+                            color: Styles.colors.text,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              /*
+                              if (card.quantity < cart.product.stock)
+                               */
+                              cart.quantity++;
+                              _total = NumberFormatTool.formatPrice(
+                                  shoppingCard.total);
+                            });
+                          },
+                          icon: Icon(
+                            Icons.add,
+                            color: Styles.colors.text,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete_forever),
+                    color: Styles.colors.unSelected,
+                    onPressed: () {
+                      setState(() => _loading = true);
+                      Request.deleteShoppingCard(cart.id).then((hasException) {
+                        getData();
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Positioned.fill(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 100.0),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(Styles.cardRadius),
+                      onTap: () => Navigator.pushNamed(
+                          context, 'product/${cart.product.id}'),
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ]),
     );
+
+//    return Padding(
+//      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+//      child: Stack(
+//        children: [
+//          Container(
+//            height: 125.0,
+//            child: Row(
+//              crossAxisAlignment: CrossAxisAlignment.stretch,
+//              children: [
+//                Container(
+//                    width: 105,
+//                    decoration: BoxDecoration(
+//                      color: Colors.white,
+//                      borderRadius: BorderRadius.circular(Styles.cardRadius),
+//                    ),
+//                    child: cart.product.pictures.isEmpty
+//                        ? null
+//                        : ClipRRect(
+//                        borderRadius:
+//                        BorderRadius.circular(Styles.cardRadius),
+//                        child: Image.network(cart.product.pictures[0]))),
+////                Expanded(
+////                    child: Padding(
+////                      padding: const EdgeInsets.only(
+////                          left: 10.0, top: 20.0, bottom: 20.0),
+////                      child: Column(
+////                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+////                        crossAxisAlignment: CrossAxisAlignment.stretch,
+////                        children: [
+////                          Text(
+////                            cart.product.name,
+////                            style: TextStyle(
+////                              fontSize: 18.0,
+////                              color: Styles.colors.text,
+////                              fontWeight: FontWeight.w400,
+////                            ),
+////                            overflow: TextOverflow.ellipsis,
+////                          ),
+////                          Text(
+////                            "$price€ / mois",
+////                            style: TextStyle(
+////                              fontSize: 18.0,
+////                              color: Styles.colors.text,
+////                              fontWeight: FontWeight.w700,
+////                            ),
+////                          ),
+////                        ],
+////                      ),
+////                    )),
+////                Container(
+////                  width: 50,
+////                  child: Column(
+////                    mainAxisAlignment: MainAxisAlignment.end,
+////                    crossAxisAlignment: CrossAxisAlignment.stretch,
+////                    children: [
+////                      Expanded(
+////                          child: Row(
+////                            children: [
+////                              Container(
+////                                child: IconButton(
+////                                  onPressed: () {
+////                                    setState(() {
+////                                      cart.quantity--;
+////                                    });
+////                                  },
+////                                  icon: Icon(Icons.remove, color: Styles.colors.text,),
+////                                ),
+////                              ),
+////                              Container(
+////                                height: 17,
+////                                padding: const EdgeInsets.symmetric(
+////                                    horizontal: 5.0),
+////                                decoration: BoxDecoration(
+////                                  color: Styles.colors.main,
+////                                  borderRadius: BorderRadius.all(
+////                                      Radius.circular(32.5)),
+////                                ),
+////                                child: Center(
+////                                  child: Text(
+////                                    cart.quantity.toString(),
+////                                    style: TextStyle(
+////                                      color: Styles.colors.text,
+////                                      fontSize: 12,
+////                                      fontWeight: FontWeight.w900,
+////                                    ),
+////                                  ),
+////                                ),
+////                              ),
+////                              Container(
+////                                child: IconButton(
+////                                  onPressed: () {
+////                                    setState(() {
+////                                      cart.quantity++;
+////                                    });
+////                                  },
+////                                  icon: Icon(Icons.remove, color: Styles.colors.text,),
+////                                ),
+////                              ),
+////                            ],
+////                          ),
+////                      ),
+////                      Expanded(
+////                        child: IconButton(
+////                          icon: Icon(Icons.delete_forever),
+////                          color: Styles.colors.unSelected,
+////                          onPressed: () {
+////                            setState(() => _loading = true);
+////                            Request.deleteShoppingCard(cart.id)
+////                                .then((hasException) {
+////                              getData();
+////                            });
+////                          },
+////                        ),
+////                      ),
+//                    ],
+//                  ),
+//                ),
+//              ],
+//          //  ),
+//          ),
+////          Positioned.fill(
+////            child: Padding(
+////              padding: const EdgeInsets.only(right: 40.0),
+////              child: Material(
+////                color: Colors.transparent,
+////                child: InkWell(
+////                  borderRadius: BorderRadius.circular(Styles.cardRadius),
+////                  onTap: () =>
+////                      Navigator.pushNamed(
+////                          context, 'product/${cart.product.id}'),
+////                ),
+////              ),
+////            ),
+////          ),
+////        ],
+////      ),
+//    );
   }
 
   @override
